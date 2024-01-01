ab2f44a2a60bf28322380e9274257f3414971876
Görkem <Gkrem33@gmail.com>
2024-01-01 14:56:28 +0300

    TypeScript interface type

---
+++ src/types/index.d.ts
@@ -0,0 +1,20 @@
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  vote_count: number;
+  release_date: string;
+  video: boolean;
+}
+export interface Category {
+  id: number;
+  name: string;
+}
