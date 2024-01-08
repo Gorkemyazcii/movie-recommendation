fdce5f8839d1dfd5de95b4fc90994c25745942cb
Görkem <Gkrem33@gmail.com>
2024-01-08 13:56:07 +0300

    default error message

---
+++ src/app/movie/[id]/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+export default function Error() {
+  return (
+    <div className="h-screen flex items-center justify-center flex-col">
+      <h1 className="text-2xl mb-2">Sayfa Error verdi</h1>
+      <Link href="/" className="underline text-xl mt-2">
+        Go Home
+      </Link>
+    </div>
+  );
+}
