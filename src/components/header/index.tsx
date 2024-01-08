ddaf1c47642efa71332f022e831f5979bf6e7554
Görkem <Gkrem33@gmail.com>
2024-01-08 13:59:56 +0300

    header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaPlayCircle } from "react-icons/fa";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="text-2xl mb-20">
+      <div className="flex justify-between items-center mt-3">
+        <Link href="/" className="flex items-center space-x-1  ">
+          <FaPlayCircle /> <div>GORKEM</div>
+        </Link>
+        <nav className="mr-10 flex space-x-10 text-lg">
+          <Link href="#">Movies</Link>
+          <Link href="#">Anime</Link>
+          <Link href="#">Series</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
