2103feb15b3f57a18c576ed52de20a3cc2d391f8
Görkem <Gkrem33@gmail.com>
2024-01-08 13:59:41 +0300

    footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="bottom-0 flex w-full h-6 mt-20 bg-zinc-600 text-black justify-center items-center rounded-[35px]">
+      Yazıcı
+    </div>
+  );
+}
