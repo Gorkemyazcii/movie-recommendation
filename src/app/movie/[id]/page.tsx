4c0e22bd15fc2e6a5463573fe566550a6ceaccfc
Görkem <Gkrem33@gmail.com>
2024-01-09 17:55:52 +0300

    fixed

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -2,13 +2,7 @@ import React from "react";
 import { notFound } from "next/navigation";
 
 import MovieContainer from "@/containers/movie";
-import Movies from "@/mocks/movies.json";
-import { getPopularMovies, getTopRatedMovies, getMovie } from "@/api";
-import { Movie } from "@/types";
-
-// async function delay(ms: number) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+import { getMovie } from "@/api";
 
 export default async function MoviePage({
   params,
@@ -17,18 +11,7 @@ export default async function MoviePage({
   params: { id: string };
   searchParams: { error: string };
 }) {
-  // const popularPromise = getPopularMovies();
-  // const topRatedPromise = getTopRatedMovies();
   const movieDetail = await getMovie(params.id);
-
-  // const [{ results: popularMovies }, { results: topRatedMovies }] =
-  //   await Promise.all([popularPromise, topRatedPromise]);
-
-  // const movieDetail: [] =
-  //   popularMovies.find((movie: Movie) => movie.id.toString() === params.id) ||
-  //   topRatedMovies.find((movie: Movie) => movie.id.toString() === params.id);
-  // const movieDetail = await getMovie(params.id);
-  // console.log(movieDetail);
 
   if (!movieDetail) {
     notFound();
@@ -39,5 +22,3 @@ export default async function MoviePage({
   }
   return <MovieContainer movie={movieDetail} />;
 }
-
-// export const movieBgPath = atom(movieDetail.poster_path);
