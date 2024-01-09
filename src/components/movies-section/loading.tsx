042ce797780d769520b51eb9d283748b6adce37b
Görkem <Gkrem33@gmail.com>
2024-01-09 17:37:44 +0300

    main loading

---
+++ src/components/movies-section/loading.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Skeleton from "@/components/skeleton";
+
+export default function MovieSectionLoading() {
+  return (
+    <div className="mt-9">
+      <Skeleton width={128} height={36} />
+      <div className="flex gap-10 justify-between">
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+        <Skeleton width={500} height={250} />
+      </div>
+    </div>
+  );
+}
