b3c9e08691f4dcf8338c151d0e88b93adf10d534
Görkem <Gkrem33@gmail.com>
2024-01-18 18:28:11 +0300

    New Header design

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { FaPlayCircle } from "react-icons/fa";
+import { FaFilm, FaPlayCircle } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="text-2xl mb-20 max-md:text-lg">
       <div className="flex justify-between items-center mt-3">
-        <Link href="/" className="flex items-center space-x-1  ">
-          <FaPlayCircle /> <div>GORKEM</div>
+        <Link href="/" className="flex items-center space-x-1 text-black ">
+          <FaFilm />
+          <div className="text-nowrap bg-gradient-to-l to-stone-300 from-emerald-600 bg-clip-text text-transparent">
+            GORKEM FILM
+          </div>
         </Link>
         <nav className="mr-10 flex space-x-10 text-lg max-md:text-sm">
           <Link href="https://github.com/Gorkemyazcii">Github</Link>
